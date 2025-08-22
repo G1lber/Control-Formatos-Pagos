a7161c63098672f93acab2fe5897cc688cf818dc
G1lber <andresargote300@gmail.com>
2025-08-21 19:24:27 -0500

    Modales de recuperación de password

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from "./pages/Login";
 import Inicio from './pages/Inicio';
 import Formulario from './pages/Formulario';
+import RecuperarPassword from "./pages/RecuperarPassword";
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <Route path="/" element={<Inicio />} />
       <Route path="/formulario" element={<Formulario />} />
       <Route path="/login" element={<Login />} />
-     
+      <Route path="/recuperar-password" element={<RecuperarPassword />} />
+      {/* Puedes agregar más rutas aquí según sea necesario */}
     </Routes>
   );
 }
