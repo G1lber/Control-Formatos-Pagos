bb911ce4322c636d1a7f468957f202f65dd89fb2
G1lber <martinezfrancogilber@gmail.com>
2025-09-02 11:09:42 -0500

    Cambio en caso de que sea necesario ver en el celular

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,5 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
+  server: {
+    host: '0.0.0.0',    // Escucha en todas las interfaces
+    port: 5173,         // Puerto (puedes cambiarlo si quieres)
+  }
   
 })
+
+
