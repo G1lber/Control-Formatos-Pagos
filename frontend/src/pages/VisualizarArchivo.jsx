781cd8897c0df52bbfb670c6732d2240b9bacf19
G1lber <martinezfrancogilber@gmail.com>
2025-09-02 18:07:42 -0500

    Cambios completos

--- frontend/src/pages/VisualizarArchivo.jsx
+++ frontend/src/pages/VisualizarArchivo.jsx
@@ -102,7 +102,10 @@ export default function VisualizarArchivo() {
           {/* Botones de acción */}
           <div className="flex justify-end gap-3 p-4 border-t bg-gray-50">
             <button 
-            onClick={() => setShowModal(true)}
+            onClick={() => {
+              setTipoArchivo(tipo === "gf" ? "archivo1" : "archivo2"); // según la ruta
+              setShowModal(true)
+            }}
             className="px-4 py-2 rounded-md bg-red-600 text-white hover:bg-red-700">
               Rechazar
             </button>
